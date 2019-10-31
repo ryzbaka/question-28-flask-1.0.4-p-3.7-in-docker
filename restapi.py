3dceaebc010b256f3db61d21141f9672aff515d6
Hamza Ali Rizvi <hamzaali151214@hotmail.com>
2019-10-31 12:44:16 +0530

    solved

--- restapi.py
+++ restapi.py
@@ -1,11 +1,19 @@
-from flask import Flask
+import json
+from flask import Flask,jsonify,request
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
     return "Hello World!"
-
+@app.route("/api/add",methods=["POST"])
+def addtwo():
+    num1=request.json["number1"]
+    num2=request.json["number2"]
+    sum=int(num1)+int(num2)
+    object={"sum":sum}
+    json_object=jsonify(object)
+    return json_object
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(port=5000)
